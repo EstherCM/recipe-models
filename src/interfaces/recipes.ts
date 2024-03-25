6e6dc00b9f471e3f1041f4c658552f3214b29c8c
EstherCM <esther.carreton8@gmail.com>
2024-03-25 18:35:23 +0100

    feat: some configs

--- src/interfaces/recipes.ts
+++ src/interfaces/recipes.ts
@@ -1,13 +1,13 @@
-import { TypeRecipe } from '../enums/type-recipe.enum';
+import { TypeRecipe } from "../enums/type-recipe.enum";
 
 export interface IRecipe {
-    name: string;
-    image: string;
-    ingredients: string[];
-    preparation: string[];
-    people: number;
-    time: number;
-    notes: string;
-    tags: string[];
-    type: TypeRecipe[];
-  }
+  name: string;
+  image: string;
+  ingredients: string[];
+  preparation: string[];
+  people: number;
+  time: number;
+  notes: string;
+  tags: string[];
+  type: TypeRecipe[];
+}
